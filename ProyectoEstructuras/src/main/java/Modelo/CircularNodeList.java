0edf1a17ebfd90f52f910935cadae6072dc581e7
IsmaelYumi <93888943+IsmaelYumi@users.noreply.github.com>
2023-06-17 16:06:13 -0500

    Creaciond e clases
    Creacion de clases lista circular doblemente enlazada , nodo doblemente enlazado circular e interfaz lista

---
+++ ProyectoEstructuras/src/main/java/Modelo/CircularNodeList.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author yumip
+ * @param <E>
+ */
+public class CircularNodeList<E> {
+    private E content;
+    private CircularNodeList<E> next;
+    private CircularNodeList<E> previous;
+    
+    
+}
---
+++ ProyectoEstructuras/src/main/java/Modelo/DoubleCircleLinkedList.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author yumip
+ * @param <E>
+ */
+public class DoubleCircleLinkedList<E> implements List<E> {
+  
+
+    @Override
+    public E getByIndex(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean remover(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(E element, int index) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean addFirst(E element) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean addLast(E element) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
