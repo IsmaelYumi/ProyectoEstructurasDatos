19232aeb442d60fc31e00a6ed8f291e059c536d9
Raul Leon <raleon@fidenslat.com>
2023-07-09 23:38:21 -0500

    avanced de login y registro

--- src/main/java/Controlador/LoguinController.java
+++ src/main/java/Controlador/LoguinController.java
@@ -8,9 +8,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import Modelo.CircularNodeList;
+import java.io.FileWriter;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -19,18 +22,46 @@ import javafx.fxml.FXML;
  */
 public class LoguinController implements Initializable {
 
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Button registarButton;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-      
+      registarButton.setOnAction(e -> {
+          try {
+              vistaRegistro();
+          } catch (IOException ex) {
+              ex.printStackTrace();
+          }
+      });
     }
-     @FXML
+     
+    @FXML     
     void Ingresar(ActionEvent event) throws IOException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
         App.setRoot("/Vistas/CreadorEmojis");
-
+        //login(username,password);
     }
     
+    private static void vistaRegistro() throws IOException {
+        App.setRoot("/Vistas/Register");
+    }
     
+    private static void login(String username ,String password) throws IOException {
+
+        if (RegisterController.getRegisteredUsers().containsKey(username) && RegisterController.getRegisteredUsers().get(username).equals(password)) {
+            System.out.println("Inicio de sesión exitoso. ¡Bienvenido!");
+            App.setRoot("/Vistas/CreadorEmojis");
+        } else {
+            System.out.println("Nombre de usuario o contraseña incorrectos.");
+        }
+    }
 }
---
+++ src/main/java/Controlador/RegisterController.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Raul Leon
+ */
+public class RegisterController implements Initializable{
+    
+    private static final String USERS_FILE = "users.txt";
+    private static Map<String, String> registeredUsers;
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Button registroButton;
+    @FXML
+    private Button atrasButton;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       registeredUsers = loadRegisteredUsers();
+       String username = usernameField.getText();
+       String password = passwordField.getText();
+       registroButton.setOnAction(e -> registerUser(username,password));
+       atrasButton.setOnAction(e -> {
+          try {
+              vistaLogin();
+          } catch (IOException ex) {
+              ex.printStackTrace();
+          }
+      });
+    }
+    
+    
+    private static void registerUser(String username ,String password) {
+        
+        if (registeredUsers.containsKey(username)) {
+            System.out.println("El usuario ya está registrado. Por favor, elija otro nombre de usuario.");
+            return;
+        }
+        
+        registeredUsers.put(username, password);
+        saveRegisteredUsers();
+
+        System.out.println("El usuario ha sido registrado exitosamente.");
+    }
+    
+    private static Map<String, String> loadRegisteredUsers() {
+        Map<String, String> users = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(USERS_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    users.put(parts[0], parts[1]);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error al cargar los usuarios registrados: " + e.getMessage());
+        }
+
+        return users;
+    }
+    
+    private static void saveRegisteredUsers() {
+        try (FileWriter fileWriter = new FileWriter(USERS_FILE)) {
+            for (Map.Entry<String, String> entry : registeredUsers.entrySet()) {
+                fileWriter.write(entry.getKey() + "," + entry.getValue() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al guardar los usuarios registrados: " + e.getMessage());
+        }
+    }
+    
+    public static Map<String, String> getRegisteredUsers() {
+        return registeredUsers;
+    }
+
+    private static void vistaLogin() throws IOException {
+        App.setRoot("/Vistas/Loguin");
+    }
+}
