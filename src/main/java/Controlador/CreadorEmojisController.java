732730c497b00050a43d07c1f164953dc7aa7240
IsmaelYumi <93888943+IsmaelYumi@users.noreply.github.com>
2023-07-12 01:45:45 -0500

    Implementacion de radiobutton

--- src/main/java/Controlador/CreadorEmojisController.java
+++ src/main/java/Controlador/CreadorEmojisController.java
@@ -13,9 +13,15 @@ import java.io.File;
 import javafx.scene.image.Image;
 import Modelo.DoubleCircleLinkedList;
 import java.util.ArrayList;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -32,24 +38,81 @@ public class CreadorEmojisController<E> implements Initializable {
      */
      @FXML
     private HBox ContenedorLista;
+    private ImageView ContenedorImagen;
      
      final CircularNodeList[] arrDisplay= new CircularNodeList[5];
+     String Lista="Caras";
+      @FXML
+    private RadioButton Bocas;
+        @FXML
+    private RadioButton Accesorios;
+     
+    @FXML
+    private RadioButton Caras;
+
+    @FXML
+    private RadioButton Cejas;
+
+    @FXML
+    private RadioButton Ojos;
+    
+    public  ToggleGroup tg = new ToggleGroup();
+
      
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-         CargarListas("accessories");
+         CargarListas("faces");
          actualizarVista();
-        //CargarListas("eyes");
-        //CargarListas("faces");
-        //CargarListas("mouth");
-        // TODO
-    }
+         Bocas.setToggleGroup(tg);
+         Caras.setToggleGroup(tg);
+         Cejas.setToggleGroup(tg);
+         Ojos.setToggleGroup(tg);
+         Accesorios.setToggleGroup(tg);
+     tg.selectedToggleProperty().addListener(new ChangeListener<Toggle>(){
+             @Override
+             public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+               RadioButton rb= (RadioButton)tg.getSelectedToggle();
+               if(rb!=null){
+                   Lista=rb.getText();
+                if(Lista.equals("Caras")){
+                    CargarListas("faces");
+                    actualizarVista();
+                }
+                if(Lista.equals("Cejas")){
+                    CargarListas("eyebrows");
+                    actualizarVista();
+                }if(Lista.equals("Accesorios")){
+                    CargarListas("accessories");
+                    actualizarVista();
+                }if(Lista.equals("Ojos")){
+                    CargarListas("eyes");
+                    actualizarVista();
+                }if(Lista.equals("Cejas")){
+                    CargarListas("eyebrows");
+                    actualizarVista();
+                }if(Lista.equals("Bocas")){
+                    CargarListas("mouth");
+                    actualizarVista();
+                }
+             
+               }
+         
+     }});
+            ContenedorImagen.setOnMouseClicked(  new EventHandler<MouseEvent>() {
+             @Override
+             public void handle(MouseEvent event) {
+                 System.out.println("hola");
+             }
+         });
+    
+     
+             }
+    
     public void CargarListas(String archivo){
         //Crea las listas de manera Visible,la lista se supone q crea Imageviewers y lso pone en el conainer de la lista que es un Hbox , pero no funciona bien
         //falta implementar 
         String Path="src/main/resources/Imagenes/"+archivo;
         DoubleCircleLinkedList<Image> ListaAccesorios=manejadorArchivos.cargarArchivos(Path);
-        
         for(int i=0; i<5;i++){
             CircularNodeList referenciaNodo=ListaAccesorios.getByIndex(i);
             arrDisplay[i]=referenciaNodo;
@@ -61,12 +124,10 @@ public class CreadorEmojisController<E> implements Initializable {
          for(int i=0;i<arrDisplay.length;i++){
              //Peligroso
              Image img=(Image) arrDisplay[i].getContent();
-             ImageView imgv= new ImageView(img);
-             ContenedorLista.getChildren().add(imgv);
+             ContenedorImagen=new ImageView(img);
+              ContenedorLista.getChildren().add(ContenedorImagen);
          }
-         
-         
-         
+              
     }
     @FXML
     void CambiarLista(ActionEvent event) {
@@ -75,10 +136,18 @@ public class CreadorEmojisController<E> implements Initializable {
         }
         
         actualizarVista();
-        
-        
-  
+         
     }
+     @FXML
+    void CambiarListaAtras(ActionEvent event){
+        for(int i =arrDisplay.length-1; i>=0;i--){
+             arrDisplay[i]=arrDisplay[i].getPrevious();
+        }
+        actualizarVista();
+    }
+    
+   
+     
     }
 
 
